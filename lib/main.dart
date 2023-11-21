2b9b3e39a067aafe264cc05c7abf1642887665b8
Russell Stevenson <russellstevensonlim2105@gmail.com>
2023-11-21 14:44:30 +0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:attendance_app/more_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'attendance_record_screen.dart';
 import 'check_in_screen.dart';
+import 'inbox_screen.dart';
 
 void main() => runApp(
   ChangeNotifierProvider(
@@ -30,6 +32,8 @@ class _HomeScreenState extends State<HomeScreen> {
   final List<Widget> _screens = [
     CheckInScreen(),
     AttendanceRecordScreen(),
+    InboxScreen(),
+    MoreScreen(),
   ];
 
   @override
@@ -37,9 +41,11 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Attendance App'),
+        backgroundColor: Colors.grey, // Set the navigation bar color
       ),
       body: _screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // Ensure items fit within the screen
         currentIndex: _currentIndex,
         onTap: (index) {
           setState(() {
@@ -56,12 +62,32 @@ class _HomeScreenState extends State<HomeScreen> {
             label: 'Attendance Record',
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.mail),
-          label: 'Inbox',
+            icon: Icon(Icons.mail),
+            label: 'Inbox',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.keyboard_control_outlined),
+            label: 'More',
           ),
         ],
       ),
     );
+  }
+}
+
+// Create a class to manage the attendance data using the Provider package
+class AttendanceData with ChangeNotifier {
+  List<String> attendanceRecords = [];
+
+  void checkIn() {
+    // Get the current time (you may need to import the 'intl' package for formatting)
+    String currentTime = DateTime.now().toLocal().toString();
+
+    // Add the check-in time to the list
+    attendanceRecords.add(currentTime);
+
+    // Notify listeners to update the UI
+    notifyListeners();
   }
 }
 
