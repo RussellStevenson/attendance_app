13ed07248a3b6b70d6b179c3f70e4e6d2c7fd8d7
Russell Stevenson <russellstevensonlim2105@gmail.com>
2023-11-22 16:38:39 +0800

    first commit

--- lib/more_screen.dart
+++ lib/more_screen.dart
@@ -3,8 +3,39 @@ import 'package:flutter/material.dart';
 class MoreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('More Screen'),
+    return Scaffold(
+
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Edit Profile'),
+            onTap: () {
+              // Handle Edit Profile option
+              Navigator.of(context).pop(); // Close the drawer or navigate
+            },
+          ),
+          ListTile(
+            title: Text('Settings'),
+            onTap: () {
+              // Handle Settings option
+              Navigator.of(context).pop(); // Close the drawer or navigate
+            },
+          ),
+          ListTile(
+            title: Text('Change Password'),
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          ListTile(
+            title: Text('Log Out'),
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+          )
+          // Add more options as needed
+        ],
+      ),
     );
   }
 }
