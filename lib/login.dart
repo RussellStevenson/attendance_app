9f88438b33721e35a6d11a080f2ce33fbde7fd93
Russell Stevenson <russellstevensonlim2105@gmail.com>
2023-12-11 16:56:05 +0800

    first commit

--- lib/login.dart
+++ lib/login.dart
@@ -1,9 +1,9 @@
-import 'package:attendance_app/user_registration.dart';
 import 'package:flutter/material.dart';
 import 'main.dart';
+import 'user_registration.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +13,33 @@ class LoginPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Add your logo image or icon here
+                  const Icon(
+                    Icons.school, // Replace with your desired icon
+                    size: 48.0,
+                    color: Colors.blue,
+                  ),
+                  const SizedBox(height: 8.0),
+                  const Text(
+                    'Attendance App',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16.0), // Add padding below the logo
             Container(
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25.0), // Adjust the radius as needed
-                color: Colors.grey[200], // Set the background color
+                borderRadius: BorderRadius.circular(25.0),
+                color: Colors.grey[200],
               ),
               child: const TextField(
                 decoration: InputDecoration(
@@ -47,17 +70,17 @@ class LoginPage extends StatelessWidget {
               onPressed: () {
                 Navigator.pushReplacement(
                   context,
-                  MaterialPageRoute(builder: (context) => const HomeScreen()), // Replace MainPage with your actual main page
+                  MaterialPageRoute(builder: (context) => const HomeScreen()),
                 );
               },
               child: const Text('Login'),
             ),
-            const SizedBox(height: 8.0), // Adjust the spacing between the button and the text link
+            const SizedBox(height: 8.0),
             TextButton(
               onPressed: () {
                 Navigator.pushReplacement(
                   context,
-                  MaterialPageRoute(builder: (context) => const RegistrationPage()), // Replace MainPage with your actual main page
+                  MaterialPageRoute(builder: (context) => const RegistrationPage()),
                 );
               },
               child: const Text('New user? Register here.'),
